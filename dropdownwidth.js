0679508588ddf2f921288f5121b692b2c43fb16a
Kevin Mazats <k.mazats@gmail.com>
2021-04-09 10:28:58 +0200

    dropdown ul max 3 columns

--- dropdownwidth.js
+++ dropdownwidth.js
@@ -4,22 +4,28 @@ document.addEventListener("DOMContentLoaded", (event) => {
 		let dropdownItemsCount = dropdown.childElementCount;
 		console.log(dropdownItemsCount);
 		let dropdownColCount;
-		if (dropdownItemsCount % 10 === 0) {
-			dropdownColCount = dropdownItemsCount / 10;
-		} else if (dropdownItemsCount < 10) {
-			dropdownColCount = 1;
+		if (dropdownItemsCount <= 30) {
+			if (dropdownItemsCount % 10 === 0) {
+				dropdownColCount = dropdownItemsCount / 10;
+			} else if (dropdownItemsCount < 10) {
+				dropdownColCount = 1;
+			} else {
+				dropdownColCount = Math.ceil(dropdownItemsCount / 10);
+			}
 		} else {
-			dropdownColCount = Math.ceil(dropdownItemsCount / 10);
+			dropdownColCount = 3;
 		}
 		console.log(dropdownColCount);
 		let dropdownWidth = dropdownColCount * 13.938;
 		dropdown.parentElement.parentElement.parentElement.style.maxWidth = `${dropdownWidth}rem`;
-		if (dropdownItemsCount >= 10) {
+		if (dropdownItemsCount > 30) {
+			let dropdownRowCount = Math.ceil(dropdownItemsCount / 3);
+			console.log(dropdownRowCount);
+			dropdown.style.height = `${dropdownRowCount * 3}rem`;
+		} else if (dropdownItemsCount >= 10) {
 			dropdown.style.height = "30rem";
-			
 		} else {
 			dropdown.style.height = `${dropdownItemsCount * 3}rem`;
-			
 		}
 	}
 });
